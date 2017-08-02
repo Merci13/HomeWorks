a20d6c5becf792c914a87198322f75fd3995b295
Merci13 <mrjorxe@gmail.com>
2017-08-02 10:02:05 -0600

    2/8/2017
    Se agregaron las funciones de remover, guardar  y mostrar en la tabla de usuarios los cambios, pero por algun motivo el de agregar usuarios a la tabla y al local storage no funcionan

--- tareaJavascript/javascript/javascript.js
+++ tareaJavascript/javascript/javascript.js
@@ -1,8 +1,14 @@
 
-
+var	users;
 $( document ).ready(function() {
-	var	users=localStorage.getItem('users')   
+		users=localStorage.getItem('users') 
 
+			/*agregar la lista con los usuarios a la tabla*/
+	for (var i = users.length - 1; i >= 0; i--) {
+		var usuario=users[i];
+		$.( '#table-usarios tr:last' ).after('<tr> <td>'+usuario[0]+' </td> <td>'+usuario[1]++' </td> <td> '+usuario[2]+'</td> </tr>');	
+		
+	}
 });
 
 
@@ -12,12 +18,23 @@ function guardar(){
 	user[1]=document.getElementById('apellido').value;
 	user[2]=document.getElementById('telf').value;
 
-	var actualesUsuarios= users;
-	/*agregar el nuevo usuario a la lista de usuarios existente*/
 
-		actualesUsuarios.push(user);
+	/*agregar el nuevo usuario a la lista de usuarios existente*/
+			var newuser=JSON.stringify(user);
+		users.push(newuser);
+		
 /*guardar la nueva lista de usuarios en el local storage*/
 	
-	localStorage.setItem("users",JSON.stringify(actualesUsuarios));
+	localStorage.setItem('users',JSON.stringify(users));
+
+
+
 	
+}
+/*Remover el ultimo usuario de la lista y el ultimo usuario del local storage */
+function remover(){
+	$( "#table-usarios tr:last").remove();
+	users=users.length -1;
+	localStorage.setItem('users',JSON.stringify(users));
+
 }
